53c0b54d85b5c86592d15c46fd66951c450e6a1d
Marcelo de Moraes Serpa <fullofcaffeine@gmail.com>
2014-02-20 20:59:06 -0600

    Pass UTF-8 string in hex format, instead of the character itself

--- get_drunk.rb
+++ get_drunk.rb
@@ -1,8 +1,10 @@
+beer_mug_in_utf_8_hex = "\xF0\x9F\x8D\xBA" #Terminals in OSX usually use UTF-8 encoding by default. Check it out with export | grep LANG
+
 def get_drunk(beverage,icantakethismany)
    puts "Gimme another #{beverage}"
    get_drunk(beverage,icantakethismany-1) if icantakethismany > 1
 end
-get_drunk("🍺",10)
+get_drunk(beer_mug_in_utf_8_hex,10)
 
 #TODO Animate someone drinking the beer in pure ASCII (ambitious)
 
